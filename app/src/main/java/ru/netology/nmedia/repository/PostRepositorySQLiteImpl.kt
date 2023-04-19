21194c0be8f2db7c3a1a5b77e4936c04d47747ad
Ilya Kamenik <kamenikii@yandex.ru>
2023-04-19 10:37:27 +0300

    mainscope

--- app/src/main/java/ru/netology/nmedia/repository/PostRepositorySQLiteImpl.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepositorySQLiteImpl.kt
@@ -12,8 +12,10 @@ import okhttp3.RequestBody.Companion.toRequestBody
 import ru.netology.nmedia.api.PostsApi
 import ru.netology.nmedia.dao.PostDao
 import ru.netology.nmedia.entity.PostEntity
+import ru.netology.nmedia.entity.toEntity
+import ru.netology.nmedia.errors.ApiError
+import ru.netology.nmedia.errors.NetworkError
 import java.io.IOException
-import java.lang.Exception
 
 /** КЛАСС, ОТВЕЧАЮЩИЙ ЗА РЕАЛИЗАЦИЮ МЕТОДОВ РАБОТЫ С ПОСТОМ, РАБОТАЕТ С СЕРВЕРОМ И БД */
 
@@ -34,38 +36,122 @@ class PostRepositorySQLiteImpl(private val postDao: PostDao) : PostRepository {
     override fun data() = postDao.getAll().map {it.map(PostEntity::toDto)}
 
     override suspend fun getAll() {
-        //запрашиваем с сервера
-        val response = PostsApi.retrofitService.getAll()
+        try {
+            //запрашиваем с сервера
+            val response = PostsApi.retrofitService.getAll()
 
-        //ловим ошибки
-        if (!response.isSuccessful) throw RuntimeException("Api error")
-        response.body() ?: throw RuntimeException("Body is null")
+            //ловим ошибки
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
 
-        //записываем данные с сервера
-        postDao.insert(response.body()!!.map {PostEntity.fromDto(it)})
+            //записываем данные с сервера
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.insert(body.toEntity())
+        } catch (e: IOException) { //ловим ошибки
+            throw NetworkError
+        }
     }
 
     override suspend fun save(post: Post) {
-        TODO("Not yet implemented")
+        try {
+            //запрашиваем с сервера
+            val response = PostsApi.retrofitService.save(post)
+
+            //ловим ошибки
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+
+            //записываем данные с сервера
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.save(PostEntity.fromDto(body))
+        } catch (e: IOException) { //ловим ошибки
+            throw NetworkError
+        }
     }
 
     override suspend fun edit(post: Post) {
-        TODO("Not yet implemented")
+        save(post)
     }
 
     override suspend fun removeById(id: Long) {
-        TODO("Not yet implemented")
+        try {
+            //запрашиваем с сервера
+            val response = PostsApi.retrofitService.removeById(id)
+
+            //ловим ошибки
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+
+            //записываем данные с сервера
+            postDao.removeById(id)
+        } catch (e: IOException) { //ловим ошибки
+            throw NetworkError
+        }
     }
 
     override suspend fun shareById(post: Post) {
-        TODO("Not yet implemented")
-    }
+        postDao.shareById(post.id)
+/*        try {
+//            //запрашиваем с сервера
+//            val response = PostsApi.retrofitService.shareById(post.id)
+//
+//
+//            //ловим ошибки
+//            if (!response.isSuccessful) {
+//                throw ApiError(response.code(), response.message())
+//            }
+//
+//            //записываем данные с сервера
+//            val body = response.body() ?: throw ApiError(response.code(), response.message())
+//            postDao.insert(PostEntity.fromDto(body))
+//        } catch (e: IOException) { //ловим ошибки
+//            throw NetworkError
+//        }
+*/    }
 
     override suspend fun viewById(post: Post) {
-        TODO("Not yet implemented")
-    }
+        postDao.viewItById(post.id)
+/*        try {
+            //запрашиваем с сервера
+            val response = PostsApi.retrofitService.viewItById(post.id)
+
+            //ловим ошибки
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+
+            //записываем данные с сервера
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.insert(PostEntity.fromDto(body))
+        } catch (e: IOException) { //ловим ошибки
+            throw NetworkError
+        }
+*/    }
 
     override suspend fun likeById(post: Post) {
-        TODO("Not yet implemented")
+        postDao.likeById(post.id)
+        try {
+            //запрашиваем с сервера
+            val response = if (post.likeByMe) {
+                PostsApi.retrofitService.dislikeById(post.id)
+            } else {
+                PostsApi.retrofitService.likeById(post.id)
+
+            }
+
+            //ловим ошибки
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+
+            //записываем данные с сервера
+            val body = response.body() ?: throw ApiError(response.code(), response.message())
+            postDao.insert(PostEntity.fromDto(body))
+        } catch (e: IOException) { //ловим ошибки
+            throw NetworkError
+        }
     }
 }
